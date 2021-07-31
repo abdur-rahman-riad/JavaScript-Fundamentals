e6161e807aa35a5dc1786ae1df1d13d916f7f287
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-08-01 00:27:15 +0600

    Comparison Operator

---
+++ comparison.js
@@ -0,0 +1,10 @@
+// Greater Then Comparison
+console.log(5 > 6);
+
+// Less then Comparison
+console.log(5 < 6);
+
+// Equal Comparison
+console.log(5 == 6);
+console.log(5 == 5);
+console.log(5 != 5);

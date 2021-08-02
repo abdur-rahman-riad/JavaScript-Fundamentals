73c5ea7c79c095dffff4d3bc66a495e11b280fdd
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-08-02 13:12:31 +0600

    Array Comparison

---
+++ array-comparison.js
@@ -0,0 +1,8 @@
+var tourPlace = ['coxbazar', 'Istanbul', 'rangamati', 'bali'];
+
+if (tourPlace[1] == 'Istanbul') {
+    console.log("I Will Visit " + tourPlace[1]);
+}
+else {
+    console.log("I Will Stay at Home");
+}
